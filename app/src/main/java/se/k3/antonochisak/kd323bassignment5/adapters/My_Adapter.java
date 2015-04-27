3ca637242defc3052d2bf3e5eb316150e0eae369
undvictor <undvictor@gmail.com>
2015-04-27 15:21:32 +0200

    logic running

--- app/src/main/java/se/k3/antonochisak/kd323bassignment5/adapters/My_Adapter.java
+++ app/src/main/java/se/k3/antonochisak/kd323bassignment5/adapters/My_Adapter.java
@@ -1,7 +1,78 @@
 package se.k3.antonochisak.kd323bassignment5.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import se.k3.antonochisak.kd323bassignment5.models.movie.Movie;
+import se.k3.antonochisak.kd323bassignment5.R;
+import se.k3.antonochisak.kd323bassignment5.helpers.StaticHelpers;
+import se.k3.antonochisak.kd323bassignment5.models.movie.Movie;
+
 /**
  * Created by Victo on 2015-04-27.
  */
-public class My_Adapter {
+public class My_Adapter extends BaseAdapter
+{
+    ArrayList<Movie> mMovies;
+    LayoutInflater mLayoutInflater;
+
+
+    public My_Adapter(ArrayList<Movie> mMovies, LayoutInflater mLayoutInflater) {
+        this.mMovies = mMovies;
+        this.mLayoutInflater = mLayoutInflater;
+    }
+
+    class ViewHolder{
+    @InjectView(R.id.poster_pic)
+      ImageView poster_pic;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+
+        if (view == null) {
+            view = mLayoutInflater.inflate(R.layout.list_movies, viewGroup, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        // Load pictures with picasso
+        Picasso.with(view.getContext())
+                .load(mMovies.get(position).getPoster())
+                //.resize(mItemWidth, mItemHeight)
+                .into(holder.poster_pic);
+        return view;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mMovies.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mMovies.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
 }
--- app/src/main/java/se/k3/antonochisak/kd323bassignment5/fragments/Fragment_my.java
+++ app/src/main/java/se/k3/antonochisak/kd323bassignment5/fragments/Fragment_my.java
@@ -1,41 +1,204 @@
 package se.k3.antonochisak.kd323bassignment5.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.MutableData;
+import com.firebase.client.Transaction;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.security.auth.callback.Callback;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import se.k3.antonochisak.kd323bassignment5.R;
+import se.k3.antonochisak.kd323bassignment5.adapters.My_Adapter;
+import se.k3.antonochisak.kd323bassignment5.api.RestClient;
 import se.k3.antonochisak.kd323bassignment5.api.model.ApiResponse;
 import se.k3.antonochisak.kd323bassignment5.models.movie.Movie;
+
+import static se.k3.antonochisak.kd323bassignment5.helpers.StaticHelpers.FIREBASE_CHILD;
+import static se.k3.antonochisak.kd323bassignment5.helpers.StaticHelpers.FIREBASE_URL;
 
 /**
  * Created by Victo on 2015-04-27.
  */
-public class Fragment_my extends Fragment
-        implements retrofit.Callback<List<ApiResponse>>, ListView.OnClickListener {
+public class Fragment_my extends Fragment implements retrofit.Callback<List<ApiResponse>>, ListView.OnItemClickListener{
+
+    //list of movies
     ArrayList<Movie> mMovies;
+
+    //this pushes to mFireBase
     HashMap<String, Object> mMovieMap;
+
+
+    RestClient mRestClient;
+    Firebase mFireBase;
+    Firebase mRef;
+
+    String mCurrentClickedMovie = "";
+
+    //Count down-timer & bool to check if it is finished
+    CountDownTimer mVoteTimer;
+    boolean mIsVoteTimerRunning = false;
+    //New adapter-init
+    My_Adapter mAdapter;
+
+    //listan
+    @InjectView(R.id.my_list)
+    ListView mMoviesList;
+
+    //
+    @InjectView(R.id.my_progress)
+    ProgressBar mProgressBar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMovies = new ArrayList<>();
+        mMovieMap = new HashMap<>();
+
+        mRestClient = new RestClient();
+        mFireBase = new Firebase(FIREBASE_URL);
+        mRef = mFireBase.child(FIREBASE_CHILD);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.my_fragment_layout,container,false);
+        // Inject views
+        ButterKnife.inject(this, view);
+
+        mAdapter = new My_Adapter(mMovies, getActivity().getLayoutInflater());
+        mMoviesList.setAdapter(mAdapter);
+
+
+        mMoviesList.setOnItemClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //listener
+        //picasso
+        mRestClient.getApiService().getPopular("images", this);
+        mProgressBar.setVisibility(View.VISIBLE);
+        initVoteTimer();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (!mIsVoteTimerRunning) {
+            voteOnMovie(position);
+            mVoteTimer.start();
+            mIsVoteTimerRunning = true;
+        }
+    }
+
+    void initVoteTimer() {
+        // So that there can only be one vote per every 3 seconds
+        mVoteTimer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                mIsVoteTimerRunning = false;
+            }
+        };
+    }
+
+    void voteOnMovie(final int i) {
+        Movie movie = mMovies.get(i);
+
+        // Very important
+        mCurrentClickedMovie = movie.getSlugline();
+
+        mMovieMap.put("title", movie.getTitle());
+        mMovieMap.put("year", movie.getYear());
+        mMovieMap.put("slugline", movie.getSlugline());
+        mMovieMap.put("poster", movie.getPoster());
+        mMovieMap.put("fanart", movie.getFanArt());
+
+        mRef.child(mCurrentClickedMovie).updateChildren(mMovieMap, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                Toast.makeText(getActivity(), "Gillade " + mMovies.get(i).getTitle(), Toast.LENGTH_SHORT).show();
+                updateVotes();
+            }
+        });
+    }
+
+    void updateVotes() {
+        mRef.child(mCurrentClickedMovie + "/votes").runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                if (mutableData.getValue() == null) {
+                    mutableData.setValue(1);
+                } else {
+                    mutableData.setValue((Long) mutableData.getValue() + 1);
+                }
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(FirebaseError firebaseError, boolean b, DataSnapshot dataSnapshot) {
+
+            }
+        });
+    }
 
 
     @Override
     public void success(List<ApiResponse> apiResponses, Response response) {
+        mProgressBar.setVisibility(View.GONE);
+        for (ApiResponse r : apiResponses) {
 
+            // Build a new movie-object for every response and add to list
+            Movie movie = new Movie.Builder()
+                    .title(r.title)
+                    .slugLine(r.ids.getSlug())
+                    .poster(r.image.getPoster().getMediumPoster())
+                    .fanArt(r.image.getFanArt().getFullFanArt())
+                    .year(r.year)
+                    .build();
+
+            mMovies.add(movie);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
     public void failure(RetrofitError error) {
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
+        if(error.getKind() == RetrofitError.Kind.NETWORK) {
+            Toast.makeText(getActivity(),
+                    getResources().getString(R.string.retrofit_network_error),
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
